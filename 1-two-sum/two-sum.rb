2948b6356544cc1b378dc45f1d23c8e2cacb5724
Raghu Boosetty <raghu.boos@gmail.com>
2024-04-13 18:20:12 +0530

    Time: 465 ms (17.48%) | Memory: 211.7 MB (88.99%) - LeetSync

--- 1-two-sum/two-sum.rb
+++ 1-two-sum/two-sum.rb
@@ -3,6 +3,7 @@
 # @return {Integer[]}
 def two_sum(nums, target)
     indexes = []
+    ## traditional approach with O(n2)
     # nums.each_with_index do |num_1, index_1|
     #     nums.each_with_index do |num_2, index_2|
     #         next if index_1.eql?(index_2)
@@ -13,18 +14,13 @@ def two_sum(nums, target)
     #     end
     #     break if indexes.size.eql?(2)
     # end
-    # indexes
+
+    # O(n)
     nums.each_with_index do |num_1, index_1|
-    # if num_1 <= target
-      num_2 = target - num_1
-      index_2 = nums.index(num_2)
-      if !index_2.nil? && (index_2 >= 0) && (index_1 != index_2)
-        indexes.push(index_1)
-        # index_1 < index_2 ? indexes.push(index_2) : indexes.unshift(index_2)
-        indexes.push(index_2)
-      end
-    # end
-    break if indexes.size.eql?(2)
-  end
+        num_2 = target - num_1
+        index_2 = nums.index(num_2)
+        indexes.push(index_1, index_2) if !index_2.nil? && (index_2 >= 0) && (index_1 != index_2)
+        break if indexes.size.eql?(2)
+    end
   indexes
 end
