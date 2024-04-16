2c65d868c4c5ac199a429cb95ae7f8768a9fa517
Raghu Boosetty <raghu.boos@gmail.com>
2024-04-16 14:02:08 +0530

    Time: 829 ms (5.11%) | Memory: 217 MB (49.40%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.rb
@@ -0,0 +1,13 @@
+# @param {String} s
+# @param {String} t
+# @return {Boolean}
+def is_anagram(s, t)
+    return false if s.size != t.size
+    s = s.chars
+    t = t.chars
+    s.each do |c| 
+        index = t.index(c)
+        index ? t.delete_at(index) : break
+    end
+    t.size.zero?
+end
