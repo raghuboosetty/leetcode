1899b3615a67c8ae68221b0f0de96ef5bb210545
Raghu Boosetty <raghu.boos@gmail.com>
2024-04-16 14:05:49 +0530

    Time: 103 ms (68.65%) | Memory: 217.5 MB (30.66%) - LeetSync

--- 242-valid-anagram/valid-anagram.rb
+++ 242-valid-anagram/valid-anagram.rb
@@ -2,6 +2,7 @@
 # @param {String} t
 # @return {Boolean}
 def is_anagram(s, t)
+    # Navie approach - O(n^n)
     # return false if s.size != t.size
     # s = s.chars
     # t = t.chars
@@ -11,5 +12,6 @@ def is_anagram(s, t)
     # end
     # t.size.zero?
 
+    # better approach - O(n^2)
     s.chars.sort.join == t.chars.sort.join
 end
